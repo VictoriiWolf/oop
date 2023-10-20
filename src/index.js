e552549e8b17b8ccaafc3e76334b583b541cf4e7
Victoria Wolf <victoriiwolf@yahoo/com>
2023-10-20 19:35:17 +0700

    homework still in process

--- src/index.js
+++ src/index.js
@@ -5,20 +5,3 @@ import { magician } from "../js.magician.js";
 import { undead } from "../js.undead.js";
 import { zombie } from "../js.zombie.js";
 import { daemon } from "../js.daemon.js";
-
-levelUp() {
-    if(this.health !== 0) {
-    this.level++;
-    this.health = 100;
-    (this.attack / 100) * 0.2;
-    (this.defence / 100) * 0.2;
-    } else {
-        throw new Error('you are dead :(')
-    }
-};
-
-damage(points) {
-    if(this.health !== 0) {
-        this.health -= points * (1 - this.defence / 100)
-    }
-}
