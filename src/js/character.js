bd1458c20f6cc6c19be7a636b5735962cd8c6c39
Victoria Wolf <victoriiwolf@yahoo/com>
2023-10-20 19:26:19 +0700

    homework still is in process

--- src/js/character.js
+++ src/js/character.js
@@ -30,5 +30,26 @@ export class Character {
             default:
                 throw new Error('This type is not correct');
         }
-    }
+    };
+
+    levelUp(health, level, attack, defence) {
+        if(this.health !== 0) {
+        this.level++;
+        this.health = 100;
+        (this.attack / 100) * 0.2;
+        (this.defence / 100) * 0.2;
+        } else {
+            throw new Error('you are dead :(')
+        }
+    };
+    
+    damage(points) {
+        if(this.health !== 0) {
+            this.health -= points * (1 - this.defence / 100)
+        }
+    };
   }
+
+  let bowman = new Character('лучник', 'Bowman', this.health, this.level, 25, 25)
+
+  console.log(bowman)
